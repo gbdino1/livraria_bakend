dc1aa3b2da4661bcf6231c730a54d12c15695084
gbdino1 <gabrielnicolas.dino@gmail.com>
2023-06-25 23:07:59 -0300

    Commit da adição de upload de arquivos

---
+++ controller/livroController.js
@@ -0,0 +1,161 @@
+const express = require('express');
+const livroModel = require('../model/livroModel');
+const upload = require('../helpers/upload/upload');
+
+/* GERENCIADOR DE ROTAS*/
+const router = express.Router();
+
+/* ROTA DE INSERÇÃO DE LIVRO(POST)*/
+router.post('/livro/inserir', upload.array('imagens', 2), (req,res)=>{
+    
+    let { titulo, preco, detalhes, tblCategoriumId } = req.body;
+    let imagem_peq = req.files[0].path;
+    let imagem_grd = req.files[1].path;
+
+    livroModel.create(
+        {
+            titulo,
+            preco,
+            imagem_peq,
+            imagem_grd,
+            detalhes,
+            tblCategoriumId
+        }
+    ).then(
+        ()=>{
+            return res.status(201).json({
+                errorStatus:false,
+                mensageStatus: 'LIVRO CADASTRADO COM SUCESSO!'
+            });
+        }
+    )
+    .catch(
+        (error)=>{
+            return res.status(500).json({
+                errorStatus:true,
+                mensageStatus: error
+            });
+        }
+    );
+
+});
+
+/* ROTA DE SELEÇÃO DE LIVRO(GET)*/
+router.get('/livro/selecionar', (req,res)=>{
+    livroModel.findAll()
+        .then(
+            (livros)=>{
+                res.json(livros);
+            }
+        )
+        .catch(
+            (error)=>{
+                return res.status(500).json({
+                    errorStatus:true,
+                    mensageStatus: error
+                });
+            }
+        );
+});
+
+/* ROTA DE SELEÇÃO DE LIVRO findByPk(GET)*/
+router.get('/livro/selecionar/:id', (req,res)=>{
+    let {id} = req.params;
+    livroModel.findByPk(id)
+        .then(
+            (livro)=>{
+                res.json(livro);
+            }
+        )
+        .catch(
+            (error)=>{
+                return res.status(500).json({
+                    errorStatus:true,
+                    mensageStatus: error
+                });
+            }
+        );
+});
+
+/* ROTA DE SELEÇÃO DE LIVRO findOne(GET)*/
+router.get('/livro/selecionar/:titulo', (req,res)=>{
+    let {titulo} = req.params;
+    livroModel.findOne({where:{titulo:titulo}})
+        .then(
+            (livro)=>{
+                res.json(livro);
+            }
+        )
+        .catch(
+            (error)=>{
+                return res.status(500).json({
+                    errorStatus:true,
+                    mensageStatus: error
+                });
+            }
+        );
+});
+
+
+/* ROTA DE ALTERAÇÃO DE LIVRO(PUT)*/
+router.put('/livro/alterar', (req,res)=>{
+
+    let {titulo, preco, imagem_grd, imagem_peq, detalhes, tblCategoriumId, id} = req.body;
+
+    livroModel.update(
+        {
+            titulo,
+            preco,
+            imagem_grd,
+            imagem_peq,
+            detalhes,
+            tblCategoriumId
+        },
+        {
+            where:{id}
+        }
+    ).then(
+        ()=>{
+            return res.status(201).json({
+                errorStatus:false,
+                mensageStatus: 'LIVRO ALTERADO COM SUCESSO!'
+            });
+        }
+    )
+    .catch(
+        (error)=>{
+            return res.status(500).json({
+                errorStatus:true,
+                mensageStatus: error
+            });
+        }
+    );
+
+})
+
+/* ROTA DE EXCLUSÃO DE LIVRO(DELETE)*/
+router.delete('/livro/excluir/:id', (req,res)=>{
+    
+    let {id} = req.params;
+
+    livroModel.destroy(
+        {where:{id}}
+    ).then(
+        ()=>{
+            return res.status(200).json({
+                errorStatus:false,
+                mensageStatus:'LIVRO EXCLUIDO COM SUCESSO'
+            });
+        }
+    ).catch(
+        (error)=>{
+            return res.status(500).json({
+                errorStatus:true,
+                mensageStatus: error
+            });
+        }
+    );
+
+})
+
+module.exports = router;
